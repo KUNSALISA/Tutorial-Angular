45b3d364e843c09ff1dca3f7e7ae7ea1ebbb4b50
KUNSALISA <attawerapathsalisa@gmail.com>
2025-07-25 22:09:29 +0700

    routing

---
+++ frontend/src/app/details/details.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationID = 0;
+
+  constructor () {
+    this.housingLocationID = Number(this.route.snapshot.params['id']);
+  }
+}
